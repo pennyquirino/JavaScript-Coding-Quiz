36edece9cfec4e01c0794a31bfb62af2e3354eb1
Money Penny <pennyquirino@gmail.com>
2020-11-21 14:56:29 -0500

    fixed questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -35,8 +35,7 @@ var questions = [
         title: "String values must be enclosed within ____ when being assigned to variables.",
         choices: ["commas", "curly brackets", "quotes", "parenthesis"],
         answer: "quotes"
-    },
-
+    }
 
 ];
 // Declared variables
