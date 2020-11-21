a21e7ff349b7af6a51853040e82d25f68ad25573
Money Penny <pennyquirino@gmail.com>
2020-11-21 14:53:56 -0500

    upload of more questions in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,35 +1,42 @@
 // list the questions for the quiz
 var questions = [
+
+    {
+        title: "A function is a set of statements that:",
+        choices: ["is true or false", "varies", "implements data types", "performs a task or calculates a value"],
+        answer: "performs a task or calculates a value"
+    },
     {
         title: "Commonly used data types DO NOT include:",
         choices: ["strings", "booleans", "alerts", "numbers"],
         answer: "alerts"
     },
-    {
-        title: "The condition in an if / else statement is enclosed within ____.",
-        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-        answer: "parentheses"
-    },
+
     {
         title: "Arrays in Javascript can be used to store ____.",
         choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
         answer: "all of the above"
     },
+
     {
-        title: "String values must be enclosed within ____ when being assigned to variables.",
-        choices: ["commas", "curly brackets", "quotes", "parenthesis"],
-        answer: "quotes"
+        title: "The condition in an if / else statement is enclosed within ____.",
+        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        answer: "parentheses"
     },
+
     {
         title: "A very useful tool for used during development and debugging for printing content to the debugger is:",
         choices: ["Javascript", "terminal / bash", "for loops", "console log"],
         answer: "console log"
     },
+  
+
     {
-        title: "A function is a set of statements that:",
-        choices: ["is true or false", "varies", "implements data types", "performs a task or calculates a value"],
-        answer: "performs a task or calculates a value"
-    }
+        title: "String values must be enclosed within ____ when being assigned to variables.",
+        choices: ["commas", "curly brackets", "quotes", "parenthesis"],
+        answer: "quotes"
+    },
+
 
 ];
 // Declared variables
